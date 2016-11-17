d120eb5487aa33d17394ad1eb95cc05e69a2d72e
Alec123445 <shageman66@gmail.com>
2016-11-17 10:21:28 -0800

    Delete template-users.php

--- DynmapSource-0.3.7.1-Web/config/template-users.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	$users = array(
-			"Admin" => array("Administrator", "admin"),
-
-			//To add more people add another array in format ---> "<Username>" => array("<Password>", "<Group>"),
-			//For now groups are only admin and user, they are not used yet, but they will be used in the future
-		)
-?>
